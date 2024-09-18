84b67cb9e7801f41ac6aabd75bb64ac30ad50f0b
Duncak46 <123555933+Duncak46@users.noreply.github.com>
2024-09-18 13:05:30 +0200

    sdfsd
    sdfdsfsd

--- Assets/Soundtracks/DoNotDestroy.cs
+++ Assets/Soundtracks/DoNotDestroy.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoNotDestroy : MonoBehaviour
 {
     public static bool inLevel = false;
     private bool JustOne = true;
     private static DoNotDestroy instance;
+
+    private Slider AudioMuch;
+    public static float audioMuch = 1;
 
     public AudioSource menu;
     public AudioSource game;
@@ -23,20 +27,40 @@ public class DoNotDestroy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        // Najdi slider jednou na zaèátku, pokud je již ve scénì
+        AudioMuch = FindObjectOfType<Slider>();
+
+        if (AudioMuch != null)
+        {
+            AudioMuch.value = audioMuch;
+            AudioMuch.onValueChanged.AddListener(OnSliderValueChanged);
+        }
     }
+
+    void OnSliderValueChanged(float value)
+    {
+        // Aktualizuj hodnotu jen pøi zmìnì ze slideru
+        audioMuch = value;
+        game.volume = audioMuch;
+        menu.volume = audioMuch;
+        boss.volume = audioMuch;
+    }
+
     void Update()
     {
-        if (inLevel == true && !JustOne)
+        if (inLevel && !JustOne)
         {
-            //Pøidat jestli je to boss nebo ne
+            // Pøidat jestli je to boss nebo ne
             game.Play();
             JustOne = true;
             menu.Stop();
         }
+
         if (!inLevel && JustOne)
         {
             menu.Play();
-            JustOne= false;
+            JustOne = false;
             game.Stop();
             boss.Stop();
         }
